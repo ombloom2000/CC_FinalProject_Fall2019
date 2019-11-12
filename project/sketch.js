6172b0548ad9f40cb6c0fd5b2a04c431f2a9b520
Olivia Bloom <ombloom2000@gmail.com>
2019-11-12 17:42:19 -0500

    adding test code for importing play library

--- project/sketch.js
+++ project/sketch.js
@@ -1,2 +1,11 @@
 //olivia bloom
 //final project fall 2019
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
